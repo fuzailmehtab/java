f192c39123aaf12c48c2c67291161d72a8de65e8
FUZAIL MEHTAB <163311874+fuzailmehtab@users.noreply.github.com>
2024-07-08 13:03:14 +0530

    Add files via upload

---
+++ library.java
@@ -0,0 +1,19 @@
+ class  Library1 {
+  String[] books;
+  int no_of_books;
+  //constructor 
+  Library1(){
+    this.books = new String[100];
+    this.no_of_books= 0;
+  }
+  public int issueBook(){
+    return 0;
+  
+  }
+  
+}
+public class library {
+  public static void main(String[]args){
+   
+  }
+}
